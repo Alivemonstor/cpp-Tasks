a019171a40987b09d0ed3ce5413fe9579062fbed
Alivemonstor [Jayden] <79489495+Alivemonstor@users.noreply.github.com>
2024-10-02 17:22:07 +0100

    start task-4

---
+++ task-4/main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	string left = R"(
+		+=-=
+		|
+		+=-=
+)";
+	string right = R"(
+		 -=+
+		   |
+		 -=+
+)";
+
+	cout << left << right;
+	return 0;
+
+}
