f22cd86a4e91d557d742e3a98e62d9f7bc138d17
Alivemonstor [Jayden] <79489495+Alivemonstor@users.noreply.github.com>
2024-10-02 19:02:04 +0100

    SO CLOSE TO FINISHING OMG

--- task-4/main.cpp
+++ task-4/main.cpp
@@ -1,20 +1,31 @@
 ﻿#include <iostream>
+#include <sstream>
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-	string left = R"(
-		+=-=
-		|
-		+=-=
-)";
-	string right = R"(
-		 -=+
-		   |
-		 -=+
-)";
+	string word;
 
-	cout << left << right;
+	cout << "Please Enter a string: ";
+	getline(cin, word);
+
+	if (word == "") {
+		cerr << "Please input a string, cannot be empty!";
+	}
+
+	if (word.length() == 1) {
+		cout << "+=-=-=+" << endl;
+		cout << "|  " << word << "  |" << endl;
+		cout << "+=-=-=+" << endl;
+	}
+	else {
+		for (char c : word) {
+			cout << "+=-=-=" << endl;
+			cout << "|  " << c << endl;
+			cout << "+=-=-=" << endl;
+		}
+	}
+
 	return 0;
 
 }
