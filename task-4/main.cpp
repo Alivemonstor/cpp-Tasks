2ffd7db1f3178cb7d6f6bb76b0ed63071b98821f
Alivemonstor [Jayden] <79489495+Alivemonstor@users.noreply.github.com>
2024-10-03 07:44:27 +0100

    task4 done

--- task-4/main.cpp
+++ task-4/main.cpp
@@ -4,28 +4,37 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	string word;
+    string user_string;
 
-	cout << "Please Enter a string: ";
-	getline(cin, word);
+    cout << "Please Enter a string: ";
+    getline(cin, user_string);
 
-	if (word == "") {
-		cerr << "Please input a string, cannot be empty!";
-	}
+    if (user_string.empty())
+    {
+        cerr << "Please input a string, cannot be empty!";
+    }
 
-	if (word.length() == 1) {
-		cout << "+=-=-=+" << endl;
-		cout << "|  " << word << "  |" << endl;
-		cout << "+=-=-=+" << endl;
-	}
-	else {
-		for (char c : word) {
-			cout << "+=-=-=" << endl;
-			cout << "|  " << c << endl;
-			cout << "+=-=-=" << endl;
-		}
-	}
+    stringstream side_lines;
+    stringstream middle_line;
 
-	return 0;
+    for (size_t index = 0; index < user_string.size(); index++)
+    {
 
+        side_lines << "+=-=-=";
+
+        if (index != 0)
+        {
+            middle_line << "  ";
+        }
+
+        middle_line << "|  " << user_string[index];
+    }
+
+    side_lines << "+" << endl;
+    middle_line << "  |" << endl;
+
+    cout << side_lines.str();
+    cout << middle_line.str();
+    cout << side_lines.str();
+    return 0;
 }
