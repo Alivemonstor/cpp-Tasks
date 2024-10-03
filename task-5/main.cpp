6a6b1dc2f649b6985df137535573e4616dbd66f2
Alivemonstor [Jayden] <79489495+Alivemonstor@users.noreply.github.com>
2024-10-03 10:28:35 +0100

    task 5 50%

--- task-5/main.cpp
+++ task-5/main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
 
 int main(int argc, char* argv[])
 {
-    //Insert your code to complete challenge 5
+    string userInput;
+
+    getline(cin, userInput);
+
+    for (char c : userInput)
+    {
+        if (!isalpha(c) && !isspace(c) && c != '.') {
+            cerr << "This string contains a non alphabetical character";
+            return 0;
+        }
+    }
+    transform(userInput.begin(), userInput.end(), userInput.begin(), toupper);
+    cout << "Upper Case: " << userInput << endl;
+
+    transform(userInput.begin(), userInput.end(), userInput.begin(), tolower);
+    cout << "Lower Case: " << userInput << endl;
+
+    cout << "Sentence Casing: ";
+
+
+
     return 0;
 }
