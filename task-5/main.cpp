6bb02550e1b51238e90cd7a2cd5d97c84e0aa776
Alivemonstor [Jayden] <79489495+Alivemonstor@users.noreply.github.com>
2024-10-03 18:47:13 +0100

    push final task 5

--- task-5/main.cpp
+++ task-5/main.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 #include <sstream>
 #include <algorithm>
+#include <cstring>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
     string userInput;
+    bool hasFound = true;
+
+	cout << "Enter a string: ";
 
     getline(cin, userInput);
 
     for (char c : userInput)
     {
-        if (!isalpha(c) && !isspace(c) && c != '.') {
+        if (!isalpha(c) && !isspace(c) && c != '.' && c != '!' && c != '?') {
             cerr << "This string contains a non alphabetical character";
             return 0;
         }
@@ -24,7 +28,44 @@ int main(int argc, char* argv[])
     cout << "Lower Case: " << userInput << endl;
 
     cout << "Sentence Casing: ";
+    for (char c : userInput)
+    {
 
+		if (isalpha(c) && hasFound)
+		{
+            cout << char(toupper(c));
+			hasFound = false;
+		}
+		else
+		{
+			cout << c;
+		}
+
+        if (c == '.' || c == '!' || c == '?')
+        {
+            hasFound = true;
+        }
+	}
+	cout << endl;
+
+    cout << "Alternate Case: ";
+    size_t index = 0;
+
+    for (char c : userInput)
+    {
+        if (index % 2 == 0)
+        {
+            cout << char(isupper(c) ? tolower(c) : toupper(c));
+        }
+        else
+        {
+            cout << c;
+        }
+
+        if (isalpha(c)) {
+            index++;
+        }
+    }
 
 
     return 0;
