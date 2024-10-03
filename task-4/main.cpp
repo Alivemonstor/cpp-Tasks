e08d9aced9435c075ea4112b4658988469b15c07
Alivemonstor [Jayden] <79489495+Alivemonstor@users.noreply.github.com>
2024-10-03 08:02:57 +0100

    finish task 4

--- task-4/main.cpp
+++ task-4/main.cpp
@@ -4,37 +4,41 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    string user_string;
+    while (true) {
+        string user_string;
 
-    cout << "Please Enter a string: ";
-    getline(cin, user_string);
+        cout << "Please Enter a string: ";
+        getline(cin, user_string);
 
-    if (user_string.empty())
-    {
-        cerr << "Please input a string, cannot be empty!";
-    }
-
-    stringstream side_lines;
-    stringstream middle_line;
-
-    for (size_t index = 0; index < user_string.size(); index++)
-    {
-
-        side_lines << "+=-=-=";
-
-        if (index != 0)
+        if (user_string.empty())
         {
-            middle_line << "  ";
+            cerr << "Please input a string, cannot be empty!" << endl;
         }
 
-        middle_line << "|  " << user_string[index];
+        stringstream side_lines;
+        stringstream middle_line;
+
+        for (size_t index = 0; index < user_string.size(); index++)
+        {
+
+            side_lines << "+=-=-=";
+
+            if (index != 0)
+            {
+                middle_line << "  ";
+            }
+
+            middle_line << "|  " << user_string[index];
+        }
+
+        side_lines << "+" << endl;
+        middle_line << "  |" << endl;
+
+        if (!user_string.empty())
+        {
+			cout << side_lines.str();
+			cout << middle_line.str();
+			cout << side_lines.str();
+        }
     }
-
-    side_lines << "+" << endl;
-    middle_line << "  |" << endl;
-
-    cout << side_lines.str();
-    cout << middle_line.str();
-    cout << side_lines.str();
-    return 0;
 }
