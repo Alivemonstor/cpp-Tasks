b6d15a482707a63abc1d79dd164abd48d8df506e
Alivemonstor [Jayden] <79489495+Alivemonstor@users.noreply.github.com>
2024-10-15 17:30:29 +0100

    finish task 7

--- task-7/main.cpp
+++ task-7/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 
+
 using namespace std;
 
 class ShopItem {
@@ -38,8 +39,10 @@ int main(int argc, char* argv[])
     const int amountOfPlayers = 3;
     int coinAmount = 100;
     int tempSelect = 0;
+    bool finalChoice = false;
 
     vector<Player> curPlayers;
+    vector<ShopItem> shopVec;
 
     Player Player1;
     curPlayers.push_back(Player1);
@@ -56,50 +59,72 @@ int main(int argc, char* argv[])
         cout << "Welcome " << curPlayers[i].Name<< endl;
     }
 
-    cout << "Welcome to the shop, at the moment you all have " << coinAmount << " coins." << endl;
+    while (finalChoice == false) {
 
-    // Creating the shop
+        cout << "Welcome to the shop, at the moment you all have " << coinAmount << " coins." << endl;
 
-    vector<ShopItem> shopVec;
+        // Creating the shop
 
-    shopVec.push_back(CreateShopItem("Great Sword", "Sword", 40, 100, "Knight"));
+        shopVec.push_back(CreateShopItem("Great Sword", "Sword", 40, 100, "Knight"));
 
-    shopVec.push_back(CreateShopItem("Scimitar", "Sword", 35, 75, "Knight"));
+        shopVec.push_back(CreateShopItem("Scimitar", "Sword", 35, 75, "Knight"));
 
-    shopVec.push_back(CreateShopItem("Dagger", "Sword", 10, 35, "Knight"));
+        shopVec.push_back(CreateShopItem("Dagger", "Sword", 10, 35, "Knight"));
 
-    shopVec.push_back(CreateShopItem("Longbow", "Bow", 20, 55, "Archer"));
+        shopVec.push_back(CreateShopItem("Longbow", "Bow", 20, 55, "Archer"));
 
-    shopVec.push_back(CreateShopItem("Crossbow", "Bow", 40, 100, "Archer"));
+        shopVec.push_back(CreateShopItem("Crossbow", "Bow", 40, 100, "Archer"));
 
-    shopVec.push_back(CreateShopItem("Rusty spear", "Spear", 10, 35, "Spearman"));
+        shopVec.push_back(CreateShopItem("Rusty spear", "Spear", 10, 35, "Spearman"));
 
-    shopVec.push_back(CreateShopItem("Iron spear", "Spear", 20, 65, "Spearman"));
+        shopVec.push_back(CreateShopItem("Iron spear", "Spear", 20, 65, "Spearman"));
 
-    for (int i = 0; i < curPlayers.size(); i++) {
+        for (int i = 0; i < curPlayers.size(); i++) {
 
-        for (int b = 0; b < shopVec.size(); b++) {
-            cout << b << ". " << shopVec[b].Name << "[" << shopVec[b].price << " coins, " << shopVec[b].damage << " damage]" << endl;
+            for (int b = 0; b < shopVec.size(); b++) {
+                cout << b << ". " << shopVec[b].Name << "[" << shopVec[b].price << " coins, " << shopVec[b].damage << " damage]" << endl;
+            }
+
+            while (curPlayers[i].ItemName == "none") {
+                cout << "You have " << coinAmount << " coins" << endl;
+                cout << curPlayers[i].Name << " Please enter the weapon you would like to buy: ";
+                cin >> tempSelect;
+
+                if (tempSelect <= shopVec.size() - 1 && tempSelect >= 0 && coinAmount >= shopVec[tempSelect].price) {
+                    curPlayers[i].ItemName = shopVec[tempSelect].Name;
+                    curPlayers[i].Role = shopVec[tempSelect].Role;
+                    curPlayers[i].damage = shopVec[tempSelect].damage;
+                    coinAmount -= shopVec[tempSelect].price;
+                    shopVec.erase(shopVec.begin() + tempSelect);
+                    cout << curPlayers[i].Name << " You are now a " << curPlayers[i].Role << " with a " << curPlayers[i].ItemName << " and you do " << curPlayers[i].damage << " damage" << endl;
+                }
+                else {
+                    cerr << "This is not a valid shop item or you cannot afford it" << endl;
+                }
+            }
         }
 
-        while (curPlayers[i].ItemName == "none") {
-            cout << "You have " << coinAmount << " coins" << endl;
-            cout << curPlayers[i].Name << " Please enter the weapon you would like to buy: ";
-            cin >> tempSelect;
+        string choice;
+        cout << "Is this your final choice? (yes/no)" << endl;
+        cin >> choice;
 
-            if (tempSelect <= shopVec.size()-1 && tempSelect >= 0 && coinAmount >= shopVec[tempSelect].price) {
-                curPlayers[i].ItemName = shopVec[tempSelect].Name;
-                curPlayers[i].Role = shopVec[tempSelect].Role;
-                curPlayers[i].damage = shopVec[tempSelect].damage;
-                coinAmount -= shopVec[tempSelect].price;
-                shopVec.erase(shopVec.begin()+tempSelect);
-                cout << curPlayers[i].Name << " You are now a " << curPlayers[i].Role << " with a " << curPlayers[i].ItemName << " and you do " << curPlayers[i].damage << " damage" << endl;
-            }
-            else {
-                cerr << "This is not a valid shop item or you cannot afford it" << endl;
+        if (choice == "yes") {
+            finalChoice = true;
+        }
+        else {
+            shopVec.clear();
+            coinAmount = 100;
+            for (int i = 0; i < curPlayers.size(); i++) {
+                curPlayers[i].ItemName = "none";
             }
         }
     }
+
+    for (int i = 0; i < curPlayers.size(); i++) {
+        cout << "Player " << i + 1 << " (" << curPlayers[i].Name << "), Role: " << curPlayers[i].Role << ", Weapon: " << curPlayers[i].ItemName << ", Damage: " << curPlayers[i].damage << endl;
+    }
+
+
 
     return 0;
 }
