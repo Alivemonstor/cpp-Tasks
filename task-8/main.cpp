20907fc470d5e76269be979f77e740cd66342e8d
Alivemonstor [Jayden] <79489495+Alivemonstor@users.noreply.github.com>
2024-10-18 12:40:24 +0100

    task 8 progression

--- task-8/main.cpp
+++ task-8/main.cpp
@@ -2,29 +2,66 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <functional>
 
 using namespace std;
 
 
-class Inventory {
+class Items {
 	public:
-		string itemName;
-		string itemDescription;
-		int itemQuantity;
+		string name;
+		string description;
+		int quantity;
 };
+
+Items AddItem(string name, string desc, int quant) {
+	Items invItem;
+
+	invItem.name = name;
+	invItem.description = desc;
+	invItem.quantity = quant;
+
+	return invItem;
+}
+
+void setItem(vector<Items>& inventory) {
+
+}
+
+void help() {
+	cout << "Here are a list of all of the commands: " << endl;
+
+
+};
+
 int main(int argc, char* argv[])
 {
+
+	map<string, function<void(vector<Items>& inventory)>> func_list {
+		{"help", help},
+		{"AddItem", setItem},
+	};
+
+	vector<Items> inventory;
+	vector<Items> itemShop;
+
     int inventorySlots;
+	string commandInput;
 
 	cout << "How many inventory slots do you want?" << endl;
 	cin >> inventorySlots;
 
-	if (!isdigit(inventorySlots)) {
+	if (cin.fail() || inventorySlots > 16) {
 		cerr << "Please enter a number." << endl;
 	}
 
-	vector<string> inventory(inventorySlots);
 
+	cout << "What would you like to do? Type help for a list of all of the commands!" << endl;
+	cin >> commandInput;
+
+	if (cin.fail() || func_list.find(commandInput) == func_list.end()) {
+		cerr << "Please enter a valid command" << endl;
+	}
 
 
 
